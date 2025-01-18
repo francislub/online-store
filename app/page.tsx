e2f189c6a5692693ef5efc97a7b0e6cfc8b3c412
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2025-01-18 15:07:56 -0800

    initial

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h2 className="text-4xl">Welcome to online store</h2>
+    </div>
+  );
+}
