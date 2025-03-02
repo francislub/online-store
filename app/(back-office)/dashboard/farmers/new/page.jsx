8e1fffe3dd54bf9b0718da08e57cd511c9ca2bd7
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2025-03-02 12:17:51 -0800

    more b

--- app/(back-office)/dashboard/farmers/new/page.jsx
+++ app/(back-office)/dashboard/farmers/new/page.jsx
@@ -11,126 +11,15 @@ import ToggleInput from '@/components/FormInputs/ToggleInput';
 import { watch } from 'lucide-react';
 import { useRouter } from "next/navigation";
 import ImageInput from '@/components/FormInputs/ImageInput';
+import NewFarmerForm from '@/components/backoffice/NewFarmerForm'
 
 export default function NewFarmer() {
   
-  const [loading, setLoading] = useState(false)
-  const {register,reset,watch,handleSubmit,formState: {errors}} = useForm({
-    defaultValues:{
-      isActive:true
-    }
-  });
-  const isActive = watch("isActive")
-
-  const router = useRouter();
-    
-    function redirect() {
-      router.push("/dashboard/farmers")
-    }
-
-  async function onSubmit(data){
-    // setLoading(true)
-    const code = generateUserCode("LFF", data.name)
-    data.code = code;
-  console.log(data);
-  makePostRequest(
-    setLoading,
-    "api/farmers",
-    data,
-    "Farmer",
-    reset
-  );
-  setImageUrl("")
-  }
   return (
     <div>
       <FormHeader title="New Farmer"/>
 
-      <form action="" onSubmit={handleSubmit(onSubmit)}
-        className="w-full max-w-4xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 mx-auto my-3"
-      >
-
-        <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
-          <TextInput
-            label="Farmer's Full Name"
-            name="name"
-            register={register}
-            errors={errors}
-            className='w-full'
-          />
-          <TextInput
-            label="Farmer's Phone"
-            name="phone"
-            type="tel"
-            register={register}
-            errors={errors}
-            className='w-full'
-          />
-          <TextInput
-            label="Farmer's Email Address"
-            name="email"
-            type="email"
-            register={register}
-            errors={errors}
-            className='w-full'
-          />
-          <TextInput
-            label="Farmer's Physical Address"
-            name="physicalAddress"
-            register={register}
-            errors={errors}
-            className='w-full'
-          />
-          <TextInput
-            label="Farmer's Contact Person"
-            name="contactPerson"
-            register={register}
-            errors={errors}
-            className='w-full'
-          />
-          <TextInput
-            label="Farmer's Contact Person Phone"
-            name="contactPersonPhone"
-            type="tel"
-            register={register}
-            errors={errors}
-            className='w-full'
-          />
-
-         <ImageInput 
-          imageUrl={logoUrl}
-          setImageUrl={setLogoUrl}
-          endpoint="farmerProfileUploader"
-          label="Farmer Profile Image"
-
-          />
-
-          <TextareaInput
-            label="Farmer's Payment Terms"
-            name="terms"
-            register={register}
-            errors={errors}
-            isRequired={false}
-          />
-          <TextareaInput
-            label="Notes"
-            name="notes"
-            register={register}
-            errors={errors}
-            isRequired={false}
-          />
-         
-         <ToggleInput 
-        label="Farmer Status" 
-        name="isActive" 
-        trueTitle="Active" 
-        falseTitle="InActive" 
-        register={register}
-        />
-        </div>
-        <SubmitButton isLoading={loading} buttonTitle="Create Farmer" loadingButtonTitle="Creating Farmer please wait..."/>
-
-      </form>
+      <NewFarmerForm />
       
     </div>
   )
---
+++ components/backoffice/NewFarmerForm.jsx
@@ -0,0 +1,134 @@
+"use client"
+
+import React, {useState} from 'react'
+import FormHeader from '@/components/backoffice/FormHeader'
+import TextInput from '@/components/FormInputs/TextInput'
+import {useForm} from "react-hook-form";
+import SubmitButton from '@/components/FormInputs/SubmitButton';
+import TextareaInput from '@/components/FormInputs/TextAreasInput';
+import { makePostRequest } from '../../../../../lib/apiRequest';
+import ToggleInput from '@/components/FormInputs/ToggleInput';
+import { watch } from 'lucide-react';
+import { useRouter } from "next/navigation";
+import ImageInput from '@/components/FormInputs/ImageInput';
+
+export default function NewFarmerForm() {
+  
+  const [loading, setLoading] = useState(false)
+  const {register,reset,watch,handleSubmit,formState: {errors}} = useForm({
+    defaultValues:{
+      isActive:true
+    }
+  });
+  const isActive = watch("isActive")
+
+  const router = useRouter();
+    
+    function redirect() {
+      router.push("/dashboard/farmers")
+    }
+
+  async function onSubmit(data){
+    // setLoading(true)
+    const code = generateUserCode("LFF", data.name)
+    data.code = code;
+  console.log(data);
+  makePostRequest(
+    setLoading,
+    "api/farmers",
+    data,
+    "Farmer",
+    reset
+  );
+  setImageUrl("")
+  }
+  return (
+
+      <form action="" onSubmit={handleSubmit(onSubmit)}
+        className="w-full max-w-4xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 mx-auto my-3"
+      >
+
+        <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+          <TextInput
+            label="Farmer's Full Name"
+            name="name"
+            register={register}
+            errors={errors}
+            className='w-full'
+          />
+          <TextInput
+            label="Farmer's Phone"
+            name="phone"
+            type="tel"
+            register={register}
+            errors={errors}
+            className='w-full'
+          />
+          <TextInput
+            label="Farmer's Email Address"
+            name="email"
+            type="email"
+            register={register}
+            errors={errors}
+            className='w-full'
+          />
+          <TextInput
+            label="Farmer's Physical Address"
+            name="physicalAddress"
+            register={register}
+            errors={errors}
+            className='w-full'
+          />
+          <TextInput
+            label="Farmer's Contact Person"
+            name="contactPerson"
+            register={register}
+            errors={errors}
+            className='w-full'
+          />
+          <TextInput
+            label="Farmer's Contact Person Phone"
+            name="contactPersonPhone"
+            type="tel"
+            register={register}
+            errors={errors}
+            className='w-full'
+          />
+
+         <ImageInput 
+          imageUrl={logoUrl}
+          setImageUrl={setLogoUrl}
+          endpoint="farmerProfileUploader"
+          label="Farmer Profile Image"
+
+          />
+
+          <TextareaInput
+            label="Farmer's Payment Terms"
+            name="terms"
+            register={register}
+            errors={errors}
+            isRequired={false}
+          />
+          <TextareaInput
+            label="Notes"
+            name="notes"
+            register={register}
+            errors={errors}
+            isRequired={false}
+          />
+         
+         <ToggleInput 
+        label="Farmer Status" 
+        name="isActive" 
+        trueTitle="Active" 
+        falseTitle="InActive" 
+        register={register}
+        />
+        </div>
+        <SubmitButton isLoading={loading} buttonTitle="Create Farmer" loadingButtonTitle="Creating Farmer please wait..."/>
+
+      </form>
+      
+  )
+}
